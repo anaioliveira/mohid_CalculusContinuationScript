9fb4e4b9500b946b8d17493918f96c986f0d04e3
Mattia Santoro <mattia.santoro@cnr.it>
2018-02-01 18:45:18 +0100

    added

--- mohid_ecopotential.sh
+++ mohid_ecopotential.sh
@@ -17,7 +17,7 @@
 #--------------- USER'S DEFINITION -----------------#
 export TERM=xterm
 echo $LD_LIBRARY_PATH
-LD_LIBRARY_PATH=/root/apps/zlib-1.2.11/lib:/root/apps/hdf5-1.8.15/lib:/root/apps/netcdf-4.4.1.1/lib
+LD_LIBRARY_PATH=/root/apps/zlib-1.2.11/lib:/root/apps/hdf5-1.8.15/lib:/root/apps/netcdf-4.4.1.1/lib:/opt/intel/compilers_and_libraries_2018.1.163/linux/compiler/lib/intel64_lin/
 echo $LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 export project_path='/root/mohidtestdocker'
